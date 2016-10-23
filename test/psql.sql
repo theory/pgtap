2dfeb3740c42f3284c34c6d3f748c7596289392f
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-10-23 00:03:49 -0500

    Support for update and parallel tests

--- test/psql.sql
+++ test/psql.sql
@@ -12,11 +12,3 @@
 -- Revert all changes on failure.
 \set ON_ERROR_ROLLBACK 1
 \set ON_ERROR_STOP true
-
-BEGIN;
-
--- Uncomment when testing with PGOPTIONS=--search_path=tap
--- CREATE SCHEMA tap; SET search_path TO tap,public;
-
--- Load the TAP functions.
-\i sql/pgtap.sql
