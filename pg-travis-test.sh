bc2e20d34ddcf2e87b9dc494a1d71cd7482cae19
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-10-23 10:49:03 -0500

    More travis fixes

--- pg-travis-test.sh
+++ pg-travis-test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Based on https://gist.github.com/petere/6023944
+
 set -eux
 
 sudo apt-get update
 
-packages="postgresql-$PGVERSION postgresql-server-dev-$PGVERSION postgresql-common"
+packages="python-setuptools postgresql-$PGVERSION postgresql-server-dev-$PGVERSION postgresql-common"
 
 # bug: http://www.postgresql.org/message-id/20130508192711.GA9243@msgid.df7cb.de
 sudo update-alternatives --remove-all postmaster.1.gz
@@ -17,14 +19,15 @@ sudo chmod a+x /etc/init.d/postgresql
 
 sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install $packages
 
-status=0
-sudo pg_createcluster --start $PGVERSION test -p 55435 -- -A trust
-
-make all
-sudo make install
 export PGPORT=55435
 export PGUSER=postgres
 export PG_CONFIG=/usr/lib/postgresql/$PGVERSION/bin/pg_config
+sudo pg_createcluster --start $PGVERSION test -p $PGPORT -- -A trust
+
+make all
+
+sudo easy_install pgxnclient
+sudo make install
 
 make regress || failed=true # Don't exit yet if this failed
 
