9f08e2d69d42632c2bc838184f1968df75ead1e3
Administrator <the_mix_room@hotmail.com>
2020-01-16 16:21:11 +0100

    Add upgrade script from 1.1.0 to 1.2.0

---
+++ sql/pgtap--1.1.0--1.2.0.sql
@@ -0,0 +1,15 @@
+-- has_view( schema, view )
+CREATE OR REPLACE FUNCTION has_view ( NAME, NAME )
+RETURNS TEXT AS $$
+    SELECT has_view ($1, $2, 
+    'View ' || quote_ident($1) || '.' || quote_ident($2) || ' should exist'
+    );    
+$$ LANGUAGE SQL;
+
+-- hasnt_view( schema, table )
+CREATE OR REPLACE FUNCTION hasnt_view ( NAME, NAME )
+RETURNS TEXT AS $$
+    SELECT hasnt_view( $1, $2,
+        'View ' || quote_ident($1) || '.' || quote_ident($2) || ' should not exist'
+    );
+$$ LANGUAGE SQL;
