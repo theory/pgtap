9659df8509e8563a1c6d55cb557f3fdd6f58f592
RampantDespair <rampantdespair@gmail.com>
2026-06-02 13:41:58 -0400

    Added beautiful symmetry to all the has and hasnts

--- sql/pgtap--1.3.4--1.3.5.sql
+++ sql/pgtap--1.3.4--1.3.5.sql
@@ -253,8 +253,26 @@ RETURNS TEXT AS $$
 $$ LANGUAGE sql;
 
 -- -----------------------------------------------------------------------------
+-- Reminder for constraitns
+-- - has_X( schema, table, description )
+-- - has_X( schema, table )
+-- - has_X( table, description )
+-- - has_X( table )
+-- - has_X( schema, table, constraint_name, description )
+-- - has_X( schema, table, constraint_name )
+-- - has_X( table, constraint_name, description )
+-- - has_X( table, constraint_name )
+-- -----------------------------------------------------------------------------
+
+-- -----------------------------------------------------------------------------
 -- Has check
 -- -----------------------------------------------------------------------------
+
+-- has_check( schema, table )
+CREATE OR REPLACE FUNCTION has_check ( NAME, NAME )
+RETURNS TEXT AS $$
+    SELECT has_check( $1, $2, 'Table ' || quote_ident($1) || '.' || quote_ident($2) || ' should have a check constraint' );
+$$ LANGUAGE sql;
 
 -- has_check( schema, table, constraint_name, description )
 CREATE OR REPLACE FUNCTION has_check ( NAME, NAME, NAME, TEXT )
@@ -284,6 +302,30 @@ $$ LANGUAGE sql;
 -- Hasnt check
 -- -----------------------------------------------------------------------------
 
+-- hasnt_check( schema, table, description )
+CREATE OR REPLACE FUNCTION hasnt_check ( NAME, NAME, TEXT )
+RETURNS TEXT AS $$
+    SELECT ok( NOT _hasc( $1, $2, 'c' ), $3 );
+$$ LANGUAGE sql;
+
+-- hasnt_check( schema, table )
+CREATE OR REPLACE FUNCTION hasnt_check ( NAME, NAME )
+RETURNS TEXT AS $$
+    SELECT hasnt_check( $1, $2, 'Table ' || quote_ident($1) || '.' || quote_ident($2) || ' should not have a check constraint' );
+$$ LANGUAGE sql;
+
+-- hasnt_check( table, description )
+CREATE OR REPLACE FUNCTION hasnt_check ( NAME, TEXT )
+RETURNS TEXT AS $$
+    SELECT ok( NOT _hasc( $1, 'c' ), $2 );
+$$ LANGUAGE sql;
+
+-- hasnt_check( table )
+CREATE OR REPLACE FUNCTION hasnt_check ( NAME )
+RETURNS TEXT AS $$
+    SELECT hasnt_check( $1, 'Table ' || quote_ident($1) || ' should not have a check constraint' );
+$$ LANGUAGE sql;
+
 -- hasnt_check( schema, table, constraint_name, description )
 CREATE OR REPLACE FUNCTION hasnt_check ( NAME, NAME, NAME, TEXT )
 RETURNS TEXT AS $$
@@ -311,6 +353,30 @@ $$ LANGUAGE sql;
 -- -----------------------------------------------------------------------------
 -- Has exclude
 -- -----------------------------------------------------------------------------
+
+-- has_exclude( schema, table, description )
+CREATE OR REPLACE FUNCTION has_exclude ( NAME, NAME, TEXT )
+RETURNS TEXT AS $$
+    SELECT ok( _hasc( $1, $2, 'x' ), $3 );
+$$ LANGUAGE sql;
+
+-- has_exclude( schema, table )
+CREATE OR REPLACE FUNCTION has_exclude ( NAME, NAME )
+RETURNS TEXT AS $$
+    SELECT has_exclude( $1, $2, 'Table ' || quote_ident($1) || '.' || quote_ident($2) || ' should have a exclude constraint' );
+$$ LANGUAGE sql;
+
+-- has_exclude( table, description )
+CREATE OR REPLACE FUNCTION has_exclude ( NAME, TEXT )
+RETURNS TEXT AS $$
+    SELECT ok( _hasc( $1, 'x' ), $2 );
+$$ LANGUAGE sql;
+
+-- has_exclude( table )
+CREATE OR REPLACE FUNCTION has_exclude ( NAME )
+RETURNS TEXT AS $$
+    SELECT has_exclude( $1, 'Table ' || quote_ident($1) || ' should have a exclude constraint' );
+$$ LANGUAGE sql;
 
 -- has_exclude( schema, table, constraint_name, description )
 CREATE OR REPLACE FUNCTION has_exclude( NAME, NAME, NAME, TEXT )
@@ -340,6 +406,30 @@ $$ LANGUAGE sql;
 -- Hasnt exclude
 -- -----------------------------------------------------------------------------
 
+-- hasnt_exclude( schema, table, description )
+CREATE OR REPLACE FUNCTION hasnt_exclude ( NAME, NAME, TEXT )
+RETURNS TEXT AS $$
+    SELECT ok( NOT _hasc( $1, $2, 'x' ), $3 );
+$$ LANGUAGE sql;
+
+-- hasnt_exclude( schema, table )
+CREATE OR REPLACE FUNCTION hasnt_exclude ( NAME, NAME )
+RETURNS TEXT AS $$
+    SELECT hasnt_exclude( $1, $2, 'Table ' || quote_ident($1) || '.' || quote_ident($2) || ' should not have a exclude constraint' );
+$$ LANGUAGE sql;
+
+-- hasnt_exclude( table, description )
+CREATE OR REPLACE FUNCTION hasnt_exclude ( NAME, TEXT )
+RETURNS TEXT AS $$
+    SELECT ok( NOT _hasc( $1, 'x' ), $2 );
+$$ LANGUAGE sql;
+
+-- hasnt_exclude( table )
+CREATE OR REPLACE FUNCTION hasnt_exclude ( NAME )
+RETURNS TEXT AS $$
+    SELECT hasnt_exclude( $1, 'Table ' || quote_ident($1) || ' should not have a exclude constraint' );
+$$ LANGUAGE sql;
+
 -- hasnt_exclude( schema, table, constraint_name, description )
 CREATE OR REPLACE FUNCTION hasnt_exclude ( NAME, NAME, NAME, TEXT )
 RETURNS TEXT AS $$
@@ -368,6 +458,12 @@ $$ LANGUAGE sql;
 -- Has foreign key
 -- -----------------------------------------------------------------------------
 
+-- has_fk( schema, table )
+CREATE OR REPLACE FUNCTION has_fk ( NAME, NAME )
+RETURNS TEXT AS $$
+    SELECT has_fk( $1, $2, 'Table ' || quote_ident($1) || '.' || quote_ident($2) || ' should have a foreign key constraint' );
+$$ LANGUAGE sql;
+
 -- has_fk( schema, table, constraint_name, description )
 CREATE OR REPLACE FUNCTION has_fk ( NAME, NAME, NAME, TEXT )
 RETURNS TEXT AS $$
@@ -395,6 +491,12 @@ $$ LANGUAGE sql;
 -- -----------------------------------------------------------------------------
 -- Hasnt foreign key
 -- -----------------------------------------------------------------------------
+
+-- hasnt_fk( schema, table )
+CREATE OR REPLACE FUNCTION hasnt_fk ( NAME, NAME )
+RETURNS TEXT AS $$
+    SELECT hasnt_fk( $1, $2, 'Table ' || quote_ident($1) || '.' || quote_ident($2) || ' should not have a foreign key constraint' );
+$$ LANGUAGE sql;
 
 -- hasnt_fk( schema, table, constraint_name, description )
 CREATE OR REPLACE FUNCTION hasnt_fk ( NAME, NAME, NAME, TEXT )
@@ -452,6 +554,12 @@ $$ LANGUAGE sql;
 -- Hasnt primary key
 -- -----------------------------------------------------------------------------
 
+-- hasnt_pk( schema, table )
+CREATE OR REPLACE FUNCTION hasnt_pk ( NAME, NAME )
+RETURNS TEXT AS $$
+    SELECT hasnt_pk( $1, $2, 'Table ' || quote_ident($1) || '.' || quote_ident($2) || ' should not have a primary key' );
+$$ LANGUAGE sql;
+
 -- hasnt_pk( schema, table, constraint_name, description )
 CREATE OR REPLACE FUNCTION hasnt_pk ( NAME, NAME, NAME, TEXT )
 RETURNS TEXT AS $$
@@ -479,6 +587,12 @@ $$ LANGUAGE sql;
 -- -----------------------------------------------------------------------------
 -- Has unique
 -- -----------------------------------------------------------------------------
+
+-- has_unique( schema, table )
+CREATE OR REPLACE FUNCTION has_unique ( NAME, NAME )
+RETURNS TEXT AS $$
+    SELECT has_unique( $1, $2, 'Table ' || quote_ident($1) || '.' || quote_ident($2) || ' should have a unique constraint' );
+$$ LANGUAGE sql;
 
 -- has_unique( schema, table, constraint_name, description )
 CREATE OR REPLACE FUNCTION has_unique ( NAME, NAME, NAME, TEXT )
@@ -508,6 +622,30 @@ $$ LANGUAGE sql;
 -- Hasnt unique
 -- -----------------------------------------------------------------------------
 
+-- hasnt_unique( schema, table, description )
+CREATE OR REPLACE FUNCTION hasnt_unique ( NAME, NAME, TEXT )
+RETURNS TEXT AS $$
+    SELECT ok( NOT _hasc( $1, $2, 'u' ), $3 );
+$$ LANGUAGE sql;
+
+-- hasnt_unique( schema, table )
+CREATE OR REPLACE FUNCTION hasnt_unique ( NAME, NAME )
+RETURNS TEXT AS $$
+    SELECT hasnt_unique( $1, $2, 'Table ' || quote_ident($1) || '.' || quote_ident($2) || ' should not have a unique constraint' );
+$$ LANGUAGE sql;
+
+-- hasnt_unique( table, description )
+CREATE OR REPLACE FUNCTION hasnt_unique ( NAME, TEXT )
+RETURNS TEXT AS $$
+    SELECT ok( NOT _hasc( $1, 'u' ), $2 );
+$$ LANGUAGE sql;
+
+-- hasnt_unique( table )
+CREATE OR REPLACE FUNCTION hasnt_unique ( NAME )
+RETURNS TEXT AS $$
+    SELECT hasnt_unique( $1, 'Table ' || quote_ident($1) || ' should not have a unique constraint' );
+$$ LANGUAGE sql;
+
 -- hasnt_unique( schema, table, constraint_name, description )
 CREATE OR REPLACE FUNCTION hasnt_unique ( NAME, NAME, NAME, TEXT )
 RETURNS TEXT AS $$
@@ -531,3 +669,4 @@ CREATE OR REPLACE FUNCTION hasnt_unique ( NAME, NAME )
 RETURNS TEXT AS $$
     SELECT hasnt_unique( $1, $2, 'Unique constraint ' || quote_ident($2) || ' should not exist on ' || quote_ident($1) );
 $$ LANGUAGE sql;
+
